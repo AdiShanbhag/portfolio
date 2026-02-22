eb776971fe6340a5486f9ffce17a37d5107469c8
Aditya Shanbhag <adi@Adityas-MacBook-Air.local>
2026-02-22 21:33:27 +1100

    Further conflicts merged

--- src/components/IntroDescription/IntroDescription.js
+++ src/components/IntroDescription/IntroDescription.js
@@ -6,11 +6,7 @@ export default function IntroDescription() {
       <div className="hud-panel briefing-panel">
         <h1 className="hero-name">Briefing</h1>
           <p>
-<<<<<<< HEAD
             With 4+ years of industry experience crafting scalable Java Spring Boot applications, 
-=======
-            With 5+ years of industry experience crafting scalable Java Spring Boot applications, 
->>>>>>> 373f3d3c8aa59f58907389a8236ecafd1cf63f07
             I’m equally passionate about gaming on PC and PS5 and diving into game development—where 
             code meets creativity.
           </p>
